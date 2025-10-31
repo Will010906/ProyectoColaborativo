5f241d938d10ef4f172be31d97d0eb1cb55011b5
Andi-7u7 <andreaurueta11@gmail.com>
2025-10-31 12:22:54 -0600

    Mensaje de prueba Andi

--- app/src/main/java/com/example/proyectocolaborativo/MainActivity.kt
+++ app/src/main/java/com/example/proyectocolaborativo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ProyectoColaborativoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Felis jawuelin grupo, soy vanessa",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
