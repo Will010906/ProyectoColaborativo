7edf971e924e7b469bff8eb30afc72d0da07fee2
IvanaD22 <diazivana798@gmail.com>
2025-11-07 12:40:10 -0600

    Modificación del mensaje

--- app/src/main/java/com/example/proyectocolaborativo/MainActivity.kt
+++ app/src/main/java/com/example/proyectocolaborativo/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ProyectoColaborativoTheme {
-        Greeting("Felis jawuelin grupo, soy vanessa e Ivana")
+        Greeting("Felis jawuelin grupo, soy vanessa e Ivana tqm")
     }
 }
